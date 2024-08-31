f833ee04114105553404092bbfdd72b0f3ad8e2f
Ramesh A <ramesharpu@gmail.com>
2024-08-31 10:16:57 +0530

    added code changes from dev1

---
+++ src/main/java/basic/Login.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Login {
+	
+	//from dev-1 + b11 branch
+	public void loginWithCredentials(String username, String password) {
+		System.out.println("username = " + username);
+		System.out.println("password = " + password);
+	}
+
+	
+}
